63f2a3c70763ea7449404991400c1bf6e86cc694
Erik Christiansen <echristiansen@ufl.edu>
2022-12-17 19:09:12 -0500

    add jquery

--- af-property-info.js
+++ af-property-info.js
@@ -1,3 +1,5 @@
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
 $("body").on("click", "#submit-address-correction-btn", async function (e) {
     e.preventDefault(); // prevent webflow defaults
     // $('#submit-address-correction-btn').val("Going...");
